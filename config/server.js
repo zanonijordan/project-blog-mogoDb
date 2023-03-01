b2e2faa97cb94b4fae1f5ba02c7b7b52d9cba358
JORDAN HENRIQUE MOREIRA ZANONI <52969396+zanonijordan@users.noreply.github.com>
2023-03-01 00:07:57 +0000

    init Strapi with mongoDB cloudinary

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4438c9321032ca859128c3242b173090'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '08b8f757-10fe-48db-88bc-9268c3039c7c'
+};
